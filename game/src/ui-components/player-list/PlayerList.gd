00a730cd0a0663411a2e89cf09c844559a2e855c
dulvui <simon.dalvai@gmail.com.com>
2020-08-22 12:00:19 +0200

    savinbf formation and selected players. player change still not working

--- game/src/ui-components/player-list/PlayerList.gd
+++ game/src/ui-components/player-list/PlayerList.gd
@@ -1,10 +1,18 @@
 extends Control
 
+signal select_player
+
 const PlayerProfile = preload("res://src/ui-components/player-profile/PlayerProfile.tscn")
 
 func add_players(players):
 	for player in players:
 		var player_profile = PlayerProfile.instance()
-		$ItemList.add_child(player_profile)
+		player_profile.connect("player_select",self,"select_player",[player])
+		$Container/ItemList.add_child(player_profile)
 		player_profile.set_up_info(player)
+		
+		
+func select_player(player):
+	print("change in lst")
+	emit_signal("select_player",[player])
 		
--- game/src/ui-components/player-profile/PlayerProfile.gd
+++ game/src/ui-components/player-profile/PlayerProfile.gd
@@ -1,30 +1,33 @@
 extends Control
 
+signal player_select
 
-func set_up_info(player):
-	print(player)
-	$Name.text = player["name"] + " " + player["surname"]
-	$Info/Position.text = player["position"]
-	$Info/Age.text = player["birth_date"]
-	$Info/Nationality.text = player["nationality"]
-	$Info/Team.text = player["team"]
+var player
+
+func set_up_info(new_player):
+	player = new_player
+	$HBoxContainer/Name.text = player["name"] + " " + player["surname"]
+	$DetailPopup/Info/Position.text = player["position"]
+	$DetailPopup/Info/Age.text = player["birth_date"]
+	$DetailPopup/Info/Nationality.text = player["nationality"]
+	$DetailPopup/Info/Team.text = player["team"]
 	
 	var tech = player["stats"]["technial"]
-	$Technical/Cross.text = str(tech["crossing"])
-	$Technical/Pass.text = str(tech["pass"])
-	$Technical/LongPass.text = str(tech["long_pass"])
-	$Technical/Tackling.text = str(tech["tackling"])
-	$Technical/Corner.text = str(tech["corners"])
+	$DetailPopup/Technical/Cross.text = str(tech["crossing"])
+	$DetailPopup/Technical/Pass.text = str(tech["pass"])
+	$DetailPopup/Technical/LongPass.text = str(tech["long_pass"])
+	$DetailPopup/Technical/Tackling.text = str(tech["tackling"])
+	$DetailPopup/Technical/Corner.text = str(tech["corners"])
 	
 	var mental = player["stats"]["mental"]
-	$Mental/Agressivity.text = str(mental["agressivity"])
-	$Mental/Anticipation.text = str(mental["aniticipation"])
-	$Mental/Decisions.text = str(mental["decisions"])
-	$Mental/Concentration.text = str(mental["concentration"])
-	$Mental/Teamwork.text = str(mental["teamwork"])
+	$DetailPopup/Mental/Agressivity.text = str(mental["agressivity"])
+	$DetailPopup/Mental/Anticipation.text = str(mental["aniticipation"])
+	$DetailPopup/Mental/Decisions.text = str(mental["decisions"])
+	$DetailPopup/Mental/Concentration.text = str(mental["concentration"])
+	$DetailPopup/Mental/Teamwork.text = str(mental["teamwork"])
 	
 	# paint stats numbers
-	for child in self.get_children():
+	for child in $DetailPopup.get_children():
 		for child_child in child.get_children():
 			if child_child is Label and child_child.text.is_valid_integer():
 				if int(child_child.text) < 11 :
@@ -34,3 +37,12 @@ func set_up_info(player):
 				else:
 					child_child.add_color_override("font_color", Color.green)
 					
+
+
+func _on_Details_pressed():
+	$DetailPopup.popup_centered()
+
+
+func _on_Select_pressed():
+	print("select in prfoile")
+	emit_signal("player_select",player)
