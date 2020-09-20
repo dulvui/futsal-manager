c9d6b9e7da2f96603366322b4cfb26da5726de12
dulvui <simon.dalvai@gmail.com.com>
2020-09-21 00:53:58 +0200

    tabs in player profile detail

--- game/src/ui-components/player-profile/PlayerProfile.gd
+++ game/src/ui-components/player-profile/PlayerProfile.gd
@@ -7,27 +7,27 @@ var player = {}
 func set_up_info(new_player):
 	player = new_player
 	$HBoxContainer/Name.text = player["name"].substr(0,1) + ". " + player["surname"]
-	$DetailPopup/Info/Position.text = player["position"]
+	$DetailPopup/TabContainer/Info/Info/Position.text = player["position"]
 	$HBoxContainer/Position.text = player["position"]
-	$DetailPopup/Info/Age.text = player["birth_date"]
-	$DetailPopup/Info/Nationality.text = player["nationality"]
-	$DetailPopup/Info/Team.text = str(player["prestige"])
+	$DetailPopup/TabContainer/Info/Info/Age.text = player["birth_date"]
+	$DetailPopup/TabContainer/Info/Info/Nationality.text = player["nationality"]
+	$DetailPopup/TabContainer/Info/Info/Team.text = str(player["prestige"])
 	$HBoxContainer/Prestige.text = str(player["prestige"])
 	
 	for key in player["mental"].keys():
-		var ui = $DetailPopup/Mental.get_node(key)
+		var ui = $DetailPopup/TabContainer/Info/Mental.get_node(key)
 		ui.text = str(player["mental"][key])
 
 	for key in player["fisical"].keys():
-		var ui = $DetailPopup/Fisical.get_node(key)
+		var ui = $DetailPopup/TabContainer/Info/Fisical.get_node(key)
 		ui.text = str(player["fisical"][key])
 		
 	for key in player["technical"].keys():
-		var ui = $DetailPopup/Technical.get_node(key)
+		var ui = $DetailPopup/TabContainer/Info/Technical.get_node(key)
 		ui.text = str(player["technical"][key])
 	
 	# paint stats numbers
-	for child in $DetailPopup.get_children():
+	for child in $DetailPopup/TabContainer/Info.get_children():
 		for child_child in child.get_children():
 			if child_child is Label and child_child.text.is_valid_integer():
 				if int(child_child.text) < 11 :
