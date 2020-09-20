47958fd07101b041fd5ded1a93c8c647d50af301
Simon Dalvai <git@s9i.cc>
2020-09-20 21:18:27 +0200

    added nteam search to player list

--- game/src/ui-components/player-list/PlayerList.gd
+++ game/src/ui-components/player-list/PlayerList.gd
@@ -5,7 +5,15 @@ signal select_player
 const PlayerProfile = preload("res://src/ui-components/player-profile/PlayerProfile.tscn")
 
 var name_search = ""
+var team_search = ""
 
+func _ready():
+	$LegaueSelect.add_item("ITALIA")
+	
+	$TeamSelect.add_item("NO_TEAM")
+	for team in DataSaver.teams:
+		if team["name"] != DataSaver.selected_team:
+			$TeamSelect.add_item(team["name"])
 func add_players():
 	for child in $ScrollContainer/ItemList.get_children():
 		child.queue_free()
@@ -64,9 +72,25 @@ func select_player(player):
 		
 func filter_player(player):
 	if name_search.length() == 0 or name_search.to_upper() in player["surname"].to_upper():
-		return true
+		if team_search.length() == 0 or team_search == player["team"]:
+			return true
+		else:
+			return false
 	return false
 
 func _on_NameSearch_text_changed(new_text):
 	name_search = new_text
+	add_all_players()
+
+
+func _on_TeamSelect_item_selected(index):
+	var teams = []
+	for team in DataSaver.teams:
+		if team["name"] != DataSaver.selected_team:
+			teams.append(team)
+	
+	if index > 0:
+		team_search = teams[index-1]["name"]
+	else:
+		team_search = ""
 	add_all_players()
