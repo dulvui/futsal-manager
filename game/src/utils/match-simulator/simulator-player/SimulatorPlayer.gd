55345029b09e461223c5e47dfbfc33830a02ac09
dulvui <simon.dalvai@gmail.com.com>
2020-09-24 15:09:09 +0200

    starting to use raycast for decision makings and ball movements

--- game/src/utils/match-simulator/simulator-player/SimulatorPlayer.gd
+++ game/src/utils/match-simulator/simulator-player/SimulatorPlayer.gd
@@ -43,12 +43,14 @@ export var shirt_color = Color.red
 
 
 var has_ball = false #probably detects when ball enters Ball detector
+var future_pos
 
 func _ready():
 	add_to_group("simulator_player")
 
 func kick_off():
 	sector_pos = start_pos
+
 
 func update_decision(team_has_ball,has_ball):
 	if pos > 0: # if not goalkeeper
@@ -70,16 +72,16 @@ func set_up(new_player,field_pos):
 	pos = field_pos
 	player = new_player
 	if player["home"]:
-		sector_pos = (field_pos+1) * 200
+		sector_pos = (field_pos+1) * 100
 	else:
-		sector_pos = 1200 -( (field_pos+1) * 200)
+		sector_pos = 1200 -( (field_pos+1) * 100)
 	current_sector = sector_pos/200
 	start_pos = sector_pos
 	
-	min_sector = sector_pos - 300
+	min_sector = sector_pos - 200
 	if min_sector < 0:
 		min_sector = 0
-	max_sector = sector_pos + 300
+	max_sector = sector_pos + 600
 	if max_sector > 1200:
 		max_sector = 1200
 	
@@ -96,10 +98,9 @@ func make_offensive_with_ball_decision():
 	var shoot_factor = check_shoot()
 	var pass_factor = check_pass() * 60
 	var move_up_factor = check_move_up() * 10
-	var move_down_factor = check_move_down() * 10
 	var wait_factor = 20
 	
-	var sum: int = shoot_factor + pass_factor + move_up_factor + move_down_factor + wait_factor
+	var sum: int = shoot_factor + pass_factor + move_up_factor + wait_factor
 	var decision_factor = randi()%sum
 	
 	if decision_factor < shoot_factor:
@@ -112,10 +113,6 @@ func make_offensive_with_ball_decision():
 		print("MOVES UP")
 		move_up()
 		emit_signal("move_up",player)
-	elif decision_factor < (shoot_factor + pass_factor + move_up_factor + move_down_factor):
-		print("MOVES DOWN")
-		move_down()
-		emit_signal("move_down",player)
 	else:
 		print("WAITS")
 		emit_signal("wait",player)
@@ -143,18 +140,13 @@ func make_goalkeeper_decision():
 func make_offensive_no_ball_decision():
 #	print(player["surname"] + " team has ball")
 	
-	var move_down_factor = check_move_down()
 	var move_up_factor = check_move_up()
-	var wait_factor = randi()%20 # make depending on player stats and tema mentality
+	var wait_factor = randi()%8 # make depending on player stats and tema mentality
 	
-	var sum = move_up_factor + move_down_factor + wait_factor
+	var sum = move_up_factor  + wait_factor
 	var decision_factor = randi()%sum
 	
 	if decision_factor < move_up_factor:
-#		print("MOVES UP")
-		move_up()
-		emit_signal("move_up",player)
-	elif decision_factor < move_up_factor + move_down_factor:
 #		print("MOVES DOWN")
 		move_down()
 		emit_signal("move_down",player)
@@ -166,16 +158,11 @@ func make_defensive_decision():
 #	print(player["surname"] + " defends")
 	
 	var move_down_factor = check_move_down()
-	var move_up_factor = check_move_up()
-	var wait_factor = randi()%20 # make depending on player stats and tema mentality
+	var wait_factor = randi()%8 # make depending on player stats and tema mentality
 	
-	var decision_factor = randi()%(move_up_factor + move_down_factor + wait_factor)
+	var decision_factor = randi()%(move_down_factor + wait_factor)
 	
-	if decision_factor < move_up_factor:
-#		print("MOVES UP")
-		emit_signal("move_up",player)
-		move_down()
-	elif decision_factor < move_up_factor + move_down_factor:
+	if decision_factor < move_down_factor:
 #		print("MOVES DOWN")
 		emit_signal("move_down",player)
 		move_up()
@@ -197,6 +184,14 @@ func check_pass():
 	#add player vision affect
 	#add pass mentality
 	var pass_factor = 1
+	
+	#check mentality if more ofensive pass or possesion
+	#check sector with best value
+	# value gets calculated by near to goal + players in sector + opponents in sector
+	# save best pass option, then can be used by pass
+#	var sector_values = []
+	
+	
 	var opponent_players_in_sector = get_parent().get_team_players_in_sector(!player["home"],sector_pos)
 	var team_players_in_sector = get_parent().get_team_players_in_sector(player["home"],sector_pos)
 	pass_factor -= opponent_players_in_sector.size() * 3
@@ -232,8 +227,8 @@ func check_move_down():
 func move_down():
 	if player["home"]:
 		sector_pos -= player["fisical"]["pace"] * 10
-	else:
-		sector_pos += player["fisical"]["pace"] * 10
+#	else:
+#		sector_pos += player["fisical"]["pace"] * 10
 	if sector_pos > max_sector:
 		sector_pos = max_sector
 	if sector_pos < min_sector:
@@ -246,10 +241,10 @@ func move_down():
 
 # special movements: cornerns, penlaties, free kicks, kick off, rimessa
 func move_up():
-	if player["home"]:
-		sector_pos += player["fisical"]["pace"] * 10
-	else:
-		sector_pos -= player["fisical"]["pace"] * 10
+#	if player["home"]:
+	sector_pos += player["fisical"]["pace"] * 10
+#	else:
+#		sector_pos -= player["fisical"]["pace"] * 10
 	if sector_pos > max_sector:
 		sector_pos = max_sector
 	if sector_pos < min_sector:
--- game/src/utils/match-simulator/visual/ball/Ball.gd
+++ game/src/utils/match-simulator/visual/ball/Ball.gd
@@ -4,3 +4,6 @@ extends Area2D
 func move_to(destination):
 	$Tween.interpolate_property(self,"position",position,destination,1,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.start()
+	
+func kick_off():
+	position = Vector2(600,300)
