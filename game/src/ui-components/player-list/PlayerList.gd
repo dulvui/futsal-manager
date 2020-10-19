cbcfc0ab8b805856ee7996712195455fea8f6bc2
dulvui <simon.dalvai@gmail.com.com>
2020-10-19 17:56:02 +0200

    created shopping cat and eye icon for player list. created detail number to show colored number depending on player value. fixed player list size flags

--- game/src/ui-components/player-list/PlayerList.gd
+++ game/src/ui-components/player-list/PlayerList.gd
@@ -58,9 +58,6 @@ func add_subs():
 	$TeamSelect.hide()
 	$LegaueSelect.hide()
 	
-	var vbox = VBoxContainer.new()
-	vbox.add_constant_override("separation", 60)
-	
 #	if filter:
 	current_players = []
 	current_page = 0
@@ -74,8 +71,7 @@ func add_subs():
 		var player_profile = PlayerProfile.instance()
 		player_profile.connect("player_select",self,"select_player",[player])
 		player_profile.set_up_info(player,info_type)
-		vbox.add_child(player_profile)
-	$CurrentPlayers.add_child(vbox)
+		$CurrentPlayers.add_child(player_profile)
 	
 		
 func add_match_players():
@@ -102,9 +98,6 @@ func add_all_players(filter):
 	for child in $CurrentPlayers.get_children():
 		child.queue_free()
 		
-	var vbox = VBoxContainer.new()
-	vbox.add_constant_override("separation", 60)
-	
 	if filter:
 		current_players = []
 		current_page = 0
@@ -116,8 +109,7 @@ func add_all_players(filter):
 		var player_profile = PlayerProfile.instance()
 		player_profile.connect("player_select",self,"select_player",[player])
 		player_profile.set_up_info(player,info_type)
-		vbox.add_child(player_profile)
-	$CurrentPlayers.add_child(vbox)
+		$CurrentPlayers.add_child(player_profile)
 	
 
 func select_player(player):
