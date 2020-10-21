e7ee7c8a2852bab152f4c17eae7b0add395eb254
dulvui <simon.dalvai@gmail.com>
2020-10-21 09:41:17 -0700

    Paginator for calendar

--- game/src/ui-components/calendar/Calendar.gd
+++ game/src/ui-components/calendar/Calendar.gd
@@ -7,18 +7,24 @@ const Day = preload("res://src/ui-components/calendar/day/Day.tscn")
 # max back and forward is full current season
 
 var current_month
+var start_month
 
 func _ready():
+	current_month = DataSaver.month
+	start_month = DataSaver.month
 	set_up()
 
 func set_up():
-	current_month = DataSaver.month
 	for child in $GridContainer.get_children():
 		child.queue_free()
 	for day in DataSaver.calendar[current_month]:
 		var calendar_day = Day.instance()
-		calendar_day.set_up(day, day["day"] == DataSaver.day)
+		calendar_day.set_up(day, current_month == start_month and day["day"] == DataSaver.day)
 		$GridContainer.add_child(calendar_day)
+		
+		
+	$Paginator/Page.text = str(current_month)
+	
 
 func get_text(day):
 	for matchz in day["matches"]:
@@ -38,3 +44,18 @@ func show_all_matches(day):
 
 func _on_Close_pressed():
 	hide()
+
+
+func _on_Prev_pressed():
+	current_month -= 1
+	if current_month < 0:
+		current_month = 0
+	set_up()
+	
+	
+
+func _on_Next_pressed():
+	current_month += 1
+	if current_month > 11:
+		current_month = 11
+	set_up()
