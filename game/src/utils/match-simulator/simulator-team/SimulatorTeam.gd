8878d4e8793f0fcedecf46f9c47d3883918e66ec
dulvui <simon.dalvai@gmail.com.com>
2020-10-27 23:43:14 +0100

    minimal setup of teams and players

--- game/src/utils/match-simulator/simulator-team/SimulatorTeam.gd
+++ game/src/utils/match-simulator/simulator-team/SimulatorTeam.gd
@@ -13,6 +13,7 @@ enum states {
 var state
 
 var players = []
+var opponent_players = []
 
 var player_control
 var player_receive
@@ -20,6 +21,41 @@ var player_support
 var player_closest_to_ball
 
 var distance_player_closest_to_ball
+
+func set_up(home_players,away_players,formation):
+	for i in home_players.size():
+		var player = Player.new()
+		player.set_up(home_players[i],i,formation)
+		players.append(player)
+	for i in away_players.size():
+		var player = Player.new()
+		player.set_up(away_players[i],i,formation)
+		players.append(player)
+	
+
+func update():
+	for player in players:
+		player.update()
+	
+#	  //this information is used frequently so it's more efficient to 
+#  //calculate it just once each frame
+#  CalculateClosestPlayerToBall();
+#
+#  //the team state machine switches between attack/defense behavior. It
+#  //also handles the 'kick off' state where a team must return to their
+#  //kick off positions before the whistle is blown
+#  m_pStateMachine->Update();
+#
+#  //now update each player
+#  std::vector<PlayerBase*>::iterator it = m_Players.begin();
+#
+#  for (it; it != m_Players.end(); ++it)
+#  {
+#    (*it)->Update();
+#  }
+
+func create_player():
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func update_wait_players():
@@ -52,4 +88,5 @@ func request_pass():
 	
 func count_players_at_home():
 	pass
+	
 
