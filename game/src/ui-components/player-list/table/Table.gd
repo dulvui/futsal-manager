76e8428a38099be1a03410f2a7acf0540ecd0c92
Simon Dalvai <git@s9i.cc>
2022-04-25 11:37:21 +0200

    optimize table sorting

--- game/src/ui-components/player-list/table/Table.gd
+++ game/src/ui-components/player-list/table/Table.gd
@@ -10,87 +10,88 @@ onready var content_container = $ScrollContainer/Content
 onready var headers_container = $Headers
 
 func _ready():
+	pass
 	# TEST
-	var test_content = [
-		{
-		"a" : "hans",
-		"b" : 10,
-		"c" : 20
-		},
-		{
-		"a" : "zuis",
-		"b" : 30,
-		"c" : 20
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		},
-		{
-		"a" : "aabb",
-		"b" : 10,
-		"c" : 90
-		}
-	]
-	set_up(test_content, 10)
+#	var test_content = [
+#		{
+#		"a" : "hans",
+#		"b" : 10,
+#		"c" : 20
+#		},
+#		{
+#		"a" : "zuis",
+#		"b" : 30,
+#		"c" : 20
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		},
+#		{
+#		"a" : "aabb",
+#		"b" : 10,
+#		"c" : 90
+#		}
+#	]
+#	set_up(test_content, 10)
 	
-func set_up(_content, _row_size):
+func set_up(_content):
 	content = _content
 	_set_up_headers()
 	_set_up_content()
@@ -106,19 +107,18 @@ func _set_up_headers():
 		headers_container.add_child(button)
 	
 func _set_up_content():
-	for child in content_container.get_children():
-		child.queue_free()
+	content_container.queue_free()
+	content_container = GridContainer.new()
+	$ScrollContainer.add_child(content_container)
 		
 	content_container.columns = headers.size()
 	for item in content:
 		for value in item.values():
 			var label = Label.new()
-			print(value)
 			label.text = str(value)
 			content_container.add_child(label)
 	
 func _sort(value):
-	print(value)
 	sorter.value = value
 	content.sort_custom(sorter, "sort_ascending")
 	_set_up_content()
