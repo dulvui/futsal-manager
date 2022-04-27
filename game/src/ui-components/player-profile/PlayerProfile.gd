092fe9be65e2615df320c4f9722d0e987d02981f
Simon Dalvai <git@s9i.cc>
2022-04-27 21:28:50 +0200

    show goalkeeper stats in PlayerProfile

--- game/src/ui-components/player-profile/PlayerProfile.gd
+++ game/src/ui-components/player-profile/PlayerProfile.gd
@@ -8,7 +8,8 @@ var player = {}
 
 func set_up_info(_player):
 	player = _player
-
+	
+	$DetailPopup/TabContainer/Info/Info/Name.text = player["surname"] + " " + player["name"]
 	$DetailPopup/TabContainer/Info/Info/Position.text = player["position"]
 	$DetailPopup/TabContainer/Info/Info/Age.text = player["birth_date"]
 	$DetailPopup/TabContainer/Info/Info/Nationality.text = player["nationality"]
@@ -39,8 +40,22 @@ func set_up_info(_player):
 		var value = DetailNumber.instance()
 		value.set_up(player["attributes"]["technical"][key])
 		$DetailPopup/TabContainer/Info/Technical.add_child(value)
+		
+	for key in player["attributes"]["goalkeeper"].keys():
+		var label = Label.new()
+		label.text = tr(key.to_upper())
+		$DetailPopup/TabContainer/Info/Goalkeeper.add_child(label)
+		var value = DetailNumber.instance()
+		value.set_up(player["attributes"]["goalkeeper"][key])
+		$DetailPopup/TabContainer/Info/Goalkeeper.add_child(value)
 
 	$DetailPopup.popup_centered()
 
 func _on_Hide_pressed():
 	$DetailPopup.hide()
+
+
+
+
+func _on_DetailPopup_hide():
+	queue_free()
