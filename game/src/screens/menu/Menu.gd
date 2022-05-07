968b80fba0c821cfd2835483a8b43f1e2a5f3576
Simon Dalvai <git@s9i.cc>
2022-05-07 15:07:26 +0200

    hide continute in menu, if no season started

--- game/src/screens/menu/Menu.gd
+++ game/src/screens/menu/Menu.gd
@@ -1,9 +1,9 @@
 extends Control
 
 
-#func _ready():
-#	if DataSaver.selected_team.length() > 0:
-#	$CenterContainer/VBoxContainer/Continue.hide()
+func _ready():
+	if DataSaver.selected_team == "":
+		$CenterContainer/VBoxContainer/Continue.hide()
 
 func _on_StartGame_pressed():
 	get_tree().change_scene("res://src/screens/create-manager/CreateManager.tscn")
