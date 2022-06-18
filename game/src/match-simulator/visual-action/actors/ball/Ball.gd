7d8a64611f5f324e17a5ed00959d2e555e2092ed
dulvui <dulvui@posteo.de>
2022-06-18 13:02:47 +0200

    add ball rotation

--- game/src/match-simulator/visual-action/actors/ball/Ball.gd
+++ game/src/match-simulator/visual-action/actors/ball/Ball.gd
@@ -2,4 +2,5 @@ extends Node2D
 
 func move(final_position, time):
 	$Tween.interpolate_property(self, "position", position, final_position, time, Tween.EASE_IN, Tween.EASE_OUT)
+	$Tween.interpolate_property(self, "rotation", rand_range(-5,5), rand_range(-5,5), time, Tween.EASE_IN, Tween.EASE_OUT)	
 	$Tween.start()
