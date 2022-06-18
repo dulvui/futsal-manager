d70420b38e1645a2106b1b7ccd30eef0fa8e71aa
dulvui <dulvui@posteo.de>
2022-06-18 13:47:23 +0200

    fix goal position for visual action

--- game/src/match-simulator/visual-action/actors/ball/Ball.gd
+++ game/src/match-simulator/visual-action/actors/ball/Ball.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-func move(final_position, time):
-	$Tween.interpolate_property(self, "position", position, final_position, time, Tween.EASE_IN, Tween.EASE_OUT)
+func move(final_position, time, is_global_position = false):
+	if is_global_position:
+		$Tween.interpolate_property(self, "global_position", position, final_position, time, Tween.EASE_IN, Tween.EASE_OUT)
+	else:
+		$Tween.interpolate_property(self, "position", position, final_position, time, Tween.EASE_IN, Tween.EASE_OUT)
 	$Tween.interpolate_property(self, "rotation", rand_range(-5,5), rand_range(-5,5), time, Tween.EASE_IN, Tween.EASE_OUT)	
 	$Tween.start()
