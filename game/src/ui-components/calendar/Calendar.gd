1a3bfd18b7ff44da92645ad941861a6a2b8d7d1d
dulvui <dulvui@posteo.de>
2022-07-21 20:07:31 +0200

    start with monday in calendar, but still ugly

--- game/src/ui-components/calendar/Calendar.gd
+++ game/src/ui-components/calendar/Calendar.gd
@@ -17,20 +17,24 @@ func set_up():
 	for child in $GridContainer.get_children():
 		child.queue_free()
 	
+	# start with monday
+	var monday_counter = 7
+	while DataSaver.calendar[current_month][monday_counter]["weekday"] != "MON":
+		var calendar_day = Day.instance()
+#		var last_month_day = DataSaver.calendar[current_month].size() - monday_counter
+#		calendar_day.set_up(DataSaver.calendar[current_month - 1][last_month_day],last_month_day , current_month - 1)
+		$GridContainer.add_child(calendar_day)
+		monday_counter -= 1
+		
+	
 	
 	for i in range(0, DataSaver.calendar[current_month].size()):
 		var calendar_day = Day.instance()
 		calendar_day.set_up(DataSaver.calendar[current_month][i], i, current_month)
 		$GridContainer.add_child(calendar_day)
 #
-	$Paginator/Page.text = CalendarUtil.MONTHS[current_month - 1]
+	$Paginator/Page.text = CalendarUtil.MONTHS[current_month]
 	
-
-func get_text(day):
-	for matchz in day["matches"]:
-		if matchz["home"] == DataSaver.selected_team or matchz["away"] == DataSaver.selected_team:
-			return str(day["day"]) + "/" + str(day["month"]) + "/" + str(day["year"]) + matchz["home"] + " vs " + matchz["away"]
-
 
 func _on_Close_pressed():
 	hide()
