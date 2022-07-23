fb64f4741a70d4c5803c32f725ff0e4a086e1175
dulvui <dulvui@posteo.de>
2022-07-23 17:59:09 +0200

    optimize visual action

--- game/src/match-simulator/visual-action/actors/field/Field.gd
+++ game/src/match-simulator/visual-action/actors/field/Field.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 
-onready var width = $Floor.texture.get_width()
-onready var height = $Floor.texture.get_height()
+onready var width = int($Floor.texture.get_width() * scale.x)
+onready var height = int($Floor.texture.get_height() * scale.y)
 
