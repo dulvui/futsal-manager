f824e237f37e9c4df3c5f1e7a2e538e4d64e9f05
Simon Dalvai <git@s9i.cc>
2022-08-01 19:38:53 +0200

    visual-action: optimize ball tween

--- game/src/match-simulator/visual-action/actors/ball/Ball.gd
+++ game/src/match-simulator/visual-action/actors/ball/Ball.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 func move(final_position, time, is_global_position = false):
 	if is_global_position:
-		$Tween.interpolate_property(self, "global_position", global_position, final_position, time, Tween.EASE_IN, Tween.EASE_OUT)
+		$Tween.interpolate_property(self, "global_position", global_position, final_position, time, Tween.TRANS_QUART, Tween.EASE_OUT)
 	else:
-		$Tween.interpolate_property(self, "position", position, final_position, time, Tween.EASE_IN, Tween.EASE_OUT)
-	$Tween.interpolate_property(self, "rotation", rand_range(-5,5), rand_range(-5,5), time, Tween.EASE_IN, Tween.EASE_OUT)
+		$Tween.interpolate_property(self, "position", position, final_position, time, Tween.TRANS_QUART, Tween.EASE_OUT)
+	$Tween.interpolate_property(self, "rotation", rand_range(-5,5), rand_range(-5,5), time, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.start()
