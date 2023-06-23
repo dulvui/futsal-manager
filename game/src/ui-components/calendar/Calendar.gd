e5a332ca372689da3d96488c697e6489e98685f9
Simon Dalvai <git@s9i.cc>
2023-06-23 15:31:26 +0200

    calendar: UI refactoring

--- game/src/ui-components/calendar/Calendar.gd
+++ game/src/ui-components/calendar/Calendar.gd
@@ -1,12 +1,13 @@
 extends Control
 
-const Day = preload("res://src/ui-components/calendar/day/Day.tscn")
+const Day:PackedScene = preload("res://src/ui-components/calendar/day/Day.tscn")
 
+@onready
+var grid:GridContainer = $VBoxContainer/GridContainer
 
 # get current month and show in paginator
 # max back and forward is full current season
-
-var current_month
+var current_month:int
 
 
 func _ready() -> void:
@@ -15,7 +16,7 @@ func _ready() -> void:
 
 func set_up() -> void:
 	# clean grid container
-	for child in $GridContainer.get_children():
+	for child in grid.get_children():
 		child.queue_free()
 	
 	# start with monday: fill with transparent days
@@ -23,17 +24,17 @@ func set_up() -> void:
 	while DataSaver.calendar[current_month][monday_counter]["weekday"] != "MON":
 		var calendar_day = Day.instantiate()
 		calendar_day.modulate = Color(0,0,0,0)
-		$GridContainer.add_child(calendar_day)
+		grid.add_child(calendar_day)
 		monday_counter -= 1
 		
 	
 	
 	for i in range(0, DataSaver.calendar[current_month].size()):
 		var calendar_day = Day.instantiate()
+		grid.add_child(calendar_day)
 		calendar_day.set_up(DataSaver.calendar[current_month][i], i, current_month)
-		$GridContainer.add_child(calendar_day)
 #
-	$Paginator/Page.text = CalendarUtil.MONTHS[current_month]
+	$VBoxContainer/Paginator/Page.text = CalendarUtil.MONTHS[current_month]
 	
 
 func _on_Close_pressed() -> void:
--- game/src/ui-components/calendar/day/Day.gd
+++ game/src/ui-components/calendar/day/Day.gd
@@ -2,41 +2,53 @@ extends Control
 
 signal click
 
+@onready
+var color_rect:ColorRect = $ColorRect
+
+@onready
+var match_button:Button = $MarginContainer/VBoxContainer/Match
+
+@onready
+var weekday_label:Label = $MarginContainer/VBoxContainer/HBoxContainer/WeekDay
+
+@onready
+var month_day_label:Label = $MarginContainer/VBoxContainer/HBoxContainer/MonthDay
+
 # day button of calendar
 
 # on click emits signal, to show match or training popup in calendar
 # if something set, else do nothing
 
 func set_up(day, current_day, current_month) -> void:
-	$Label.text = str(current_day + 1)
+	month_day_label.text = str(current_day + 1)
 	var team_name
 	if day["matches"].size() > 0:
 		for matchz in day["matches"]:
 			if matchz != null:
 				if DataSaver.team_name == matchz["home"]:
 					team_name = matchz["away"]
-					$ColorRect.color = Color.DODGER_BLUE
+					color_rect.color = Color.DODGER_BLUE
 				elif DataSaver.team_name == matchz["away"]:
 					team_name = matchz["home"]
-					$ColorRect.color = Color.DEEP_SKY_BLUE
-		$Match.pressed.connect(_on_Match_pressed.bind(day["matches"]))
-		$Match.show()
-		$Match.text = team_name
+					color_rect.color = Color.DEEP_SKY_BLUE
+		match_button.pressed.connect(_on_Match_pressed.bind(day["matches"]))
+		match_button.show()
+		match_button.text = team_name
 	if current_day == DataSaver.date.day and DataSaver.date.month == current_month:
-		if $ColorRect.color != Color.DODGER_BLUE:
-			$ColorRect.color = Color.RED
-		elif $ColorRect.color != Color.DEEP_SKY_BLUE:
-			$ColorRect.color = Color.DARK_RED
+		if color_rect.color != Color.DODGER_BLUE:
+			color_rect.color = Color.RED
+		elif color_rect.color != Color.DEEP_SKY_BLUE:
+			color_rect.color = Color.DARK_RED
 		else:
-			$ColorRect.color = Color.LIGHT_PINK
+			color_rect.color = Color.LIGHT_PINK
 			
 	
-	$WeekDay.text = day["weekday"]
+	weekday_label.text = day["weekday"]
 
 
 func _on_Match_pressed(matches) -> void:
 	print("matchchh")
-	for child in $MatchPopup/VBoxContainer.get_children():
+	for child in match_button.get_node("Popup/VBoxContainer").get_children():
 		child.queue_free()
 		
 	for matchz in matches:
@@ -46,9 +58,9 @@ func _on_Match_pressed(matches) -> void:
 		label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		
-		$MatchPopup/VBoxContainer.add_child(label)
-	$MatchPopup.popup_centered()
+		match_button.get_node("Popup/VBoxContainer").add_child(label)
+	match_button.get_node("Popup/VBoxContainer").popup_centered()
 
 
 func _on_Close_pressed() -> void:
-	$MatchPopup.hide()
+	match_button.get_node("Popup/VBoxContainer").hide()
