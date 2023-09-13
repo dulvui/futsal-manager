e421c3d100ca40ed3936c9c393afd471eebcd33c
Simon Dalvai <git@s9i.cc>
2023-09-13 23:21:40 +0200

    create script_templates with license header

---
+++ game/script_templates/Node/with_header.gd
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+# meta-default: true
+extends _BASE_
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
