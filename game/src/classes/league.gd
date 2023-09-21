34aa7e40d1725b43f1236ab55ebdaad69dd871e2
Simon Dalvai <git@s9i.cc>
2023-09-21 22:50:51 +0200

    create league class

---
+++ game/src/classes/league.gd
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class_name League
+
+var country:String
+var id:int
+var name:int
+var players_file_name:String
+var teams:Array
