81fe107afd8cdf92280b254c1170dc61a826a604
Simon Dalvai <info@simondalvai.org>
2023-10-18 20:55:31 +0200

    player table: fix number fields

--- game/src/ui-components/player-list/table/player_table.gd
+++ game/src/ui-components/player-list/table/player_table.gd
@@ -73,7 +73,7 @@ func _set_up_content() -> void:
 		for item in current_content.slice(current_page * SIZE , (current_page * SIZE) + SIZE):
 			for header in headers:
 				var label
-				if typeof(item.attributes.get(info_type).get(header)) == 3:
+				if typeof(item.attributes.get(info_type).get(header)) == 2:
 					label = ColorNumber.instantiate()
 					label.set_up(item.attributes.get(info_type).get(header))
 				else:
