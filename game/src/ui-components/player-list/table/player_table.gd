57f71259cda97e9ffad7047aac53bc532af841c1
Simon Dalvai <info@simondalvai.org>
2023-10-19 23:41:45 +0200

    player list: ue scroll contianer instead of pages

--- game/src/ui-components/player-list/table/player_table.gd
+++ game/src/ui-components/player-list/table/player_table.gd
@@ -10,36 +10,23 @@ signal info_player(player:Player)
 const ColorNumber = preload("res://src/ui-components/color-number/color_number.tscn")
 const NameLabel = preload("res://src/ui-components/player-list/table/name-label/name_label.tscn")
 
-const SIZE:int = 10
-
-@onready var content_container = $VBoxContainer/Content
-
-@onready var pages = $VBoxContainer/HBoxContainer/Pages
-
-var current_page:int = 0
-var max_page:int = 0
+@onready var content_container = $ScrollContainer/Content
 
 var headers:Array[String]
-var content:Array[Player] # base content
-var current_content:Array[Player] # current visible content, can be filtered
+var players:Array[Player] # base content
 var info_type:String
 
 var sort_memory:Dictionary = {} # to save wich value is already sorted and how
 
 	
-func set_up(_headers:Array[String], _info_type:String, _content:Array[Player]=[]) -> void:
+func set_up(_headers:Array[String], _info_type:String, _players:Array[Player]=[]) -> void:
 	headers = _headers
 	info_type = _info_type
+	players = _players
 	
-	# if content, table is setup for first time
-	# else headers are changed
-	if not _content.is_empty():
-		content = _content
-		current_content = content
-	# set up sort memory
-	for header in headers:
-		sort_memory[header] = false
-		
+	for key in Constants.ATTRIBUTES.keys():
+		for attribute in Constants.ATTRIBUTES[key]:
+			sort_memory[key + "_" + attribute] = false
 	_set_up_content()
 	
 
@@ -62,69 +49,56 @@ func _set_up_headers() -> void:
 	content_container.add_child(lable_change)
 	
 func _set_up_content() -> void:
-	for child in content_container.get_children():
-		content_container.remove_child(child)
-	
-	_update_max_page()
 	_set_up_headers()
 	
 	content_container.columns = headers.size() + 2 # +2 for info and change button
-	if current_content.size() > 0:
-		for item in current_content.slice(current_page * SIZE , (current_page * SIZE) + SIZE):
+	
+	if players.size() > 0:
+		for player in players:
 			for header in headers:
 				var label
 				# check if number or string
-				if typeof(item.attributes.get(info_type).get(header)) == 2:
+				if typeof(player.attributes.get(info_type).get(header)) == 2:
 					label = ColorNumber.instantiate()
-					label.set_up(item.attributes.get(info_type).get(header))
+					label.set_up(player.attributes.get(info_type).get(header))
 				else:
 					label = NameLabel.instantiate()
-					label.set_text(item.get(header))
+					label.set_text(player.get(header))
 				content_container.add_child(label)
 
 			#info button
 			var button = Button.new()
 			button.text = "info"
-			button.button_down.connect(show_info.bind(item))
+			button.button_down.connect(show_info.bind(player))
 			content_container.add_child(button)
 
 			#change button
 			var button_change = Button.new()
-			button_change.text = "chose"
-			button_change.button_down.connect(change_player.bind(item))
+			button_change.text = "choose"
+			button_change.button_down.connect(change_player.bind(player))
 			content_container.add_child(button_change)
 	else :
 		var label = Label.new()
 		label.text = "NO_PLAYER_FOUND"
 		content_container.add_child(label)
-		
-	_update_pages()
-
 
 func filter(filters: Dictionary, exlusive = false) -> void:
 	if filters:
-		current_page = 0
 		var filtered_content = []
-		for item in content:
+		for player in players:
 			var filter_counter = 0
 			# because value can be empty
 			var valid_filter_counter = 0
 			for key in filters.keys():
 				var value = filters[key]
 				if value:
-					valid_filter_counter += 1
-					if exlusive:
-						if not str(value).to_upper() in str(item[key]).to_upper():
-							filter_counter += 1
-					else:
-						if str(value).to_upper() in str(item[key]).to_upper():
-							filter_counter += 1
-			if filter_counter == valid_filter_counter:
-				filtered_content.append(item)
-		current_content = filtered_content
-	else:
-		current_content = content
-	_set_up_content()
+					pass
+					# TODO create player labels with player info, that can be hidden or shown
+#					if exlusive:
+#						player.visible = not str(value).to_upper() in str(player[key]).to_upper():
+#					else:
+#						player.visible = str(value).to_upper() in str(player[key]).to_upper():
+						
 	
 
 func show_info(player:Player) -> void:
@@ -133,67 +107,12 @@ func show_info(player:Player) -> void:
 func change_player(player:Player) -> void:
 	select_player.emit(player)
 	
-func _sort(value) -> void:
-	current_content.sort_custom(func(a, b): return a[value] < b[value])
+func _sort(key:String, value:String) -> void:
+	players.sort_custom(func(a, b): return a[key][value] < b[key][value])
 	
-	sort_memory[value] = not sort_memory[value]
+	sort_memory[key][value] = not sort_memory[key][value]
 	
-	if sort_memory[value]:
-		current_content.reverse()
+	if sort_memory[key][value]:
+		players.reverse()
 	
 	_set_up_content()
-
-
-
-func _on_Next_pressed() -> void:
-	if current_page < max_page - 1:
-		current_page += 1
-		_set_up_content()
-		_update_pages()
-		
-
-func _on_next_5_pressed():
-	if current_page < max_page - 5:
-		current_page += 5
-	else:
-		current_page = max_page - 1
-	_set_up_content()
-	_update_pages()
-
-
-func _on_last_pressed():
-	current_page = max_page - 1
-	_set_up_content()
-	_update_pages()
-	
-
-func _on_first_pressed():
-	current_page = 0
-	_set_up_content()
-	_update_pages()
-	
-
-
-func _on_prev_5_pressed():
-	if current_page > 4:
-		current_page -= 5
-	else:
-		current_page = 0
-	_set_up_content()
-	_update_pages()
-
-
-func _on_Prev_pressed() -> void:
-	if current_page > 0:
-		current_page -= 1
-		_set_up_content()
-		_update_pages()
-	
-func _update_max_page() -> void:
-	max_page = current_content.size()/SIZE
-	if current_content.size() % SIZE != 0:
-		max_page += 1
-
-func _update_pages() -> void:
-	pages.text = str(current_page + 1) + "/" + str(max_page)
-	
