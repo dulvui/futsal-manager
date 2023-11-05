93224e0bc35437d918d402dd58553348f2fee3f3
Simon Dalvai <info@simondalvai.org>
2023-11-05 23:58:13 +0100

    simulaltions: create player-progress

---
+++ game/src/simulations/player-progress/player_progress.gd
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+extends Node
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
