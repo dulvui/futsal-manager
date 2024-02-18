c78dd64eeb8b5c3726b9d58a81b9586216c87e81
Simon Dalvai <info@simondalvai.org>
2024-02-18 13:07:27 +0100

    team_select: add league labels to list

--- game/src/screens/team-select/team_select.gd
+++ game/src/screens/team-select/team_select.gd
@@ -7,6 +7,8 @@ extends Control
 @onready var nations_container:HBoxContainer = $VBoxContainer/NationSelect
 @onready var team_list:VBoxContainer = $VBoxContainer/HSplitContainer/ScrollContainer/TeamList
 
+var active_league:League
+var active_team:Team
 
 func _ready() -> void:
 	for nation:String in Constants.Nations:
@@ -17,24 +19,32 @@ func _ready() -> void:
 	
 	set_teams()
 
+func show_team(league:League, team:Team) -> void:
+	active_league = league
+	active_team = team
+	pass
 
 func set_teams(nation:Constants.Nations = 0) -> void:
 	for child:Node in team_list.get_children():
 		child.queue_free()
 	
 	for league:League in Config.leagues.get_leagues_by_nation(nation):
+		var league_label:Label = Label.new()
+		league_label.text = league.name
+		team_list.add_child(league_label)
 		for team:Team in league.teams:
 			var team_button:Button = Button.new()
 			team_button.text = team.name
-			team_button.pressed.connect(team_selected.bind(league, team))
+			team_button.pressed.connect(show_team.bind(league, team))
 			team_list.add_child(team_button)
 
 func _on_nation_select(nation:String) -> void:
 	print(nation)
 	set_teams(Constants.Nations.get(nation))
 
-func team_selected(league:League, team:Team) -> void:
-	Config.select_team(league,team)
+
+func _on_select_team_pressed() -> void:
+	Config.select_team(active_league,active_team)
 	print("team saved")
 	CalendarUtil.create_calendar()
 	print("calendar created")
