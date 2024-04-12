b4fd7188f3b124dfdc6df22ac3348b5581a5b7c1
Simon Dalvai <info@simondalvai.org>
2024-04-13 01:11:22 +0200

    language picker: fix en signal and rename methods

--- game/src/screens/language-pick/language_picker.gd
+++ game/src/screens/language-pick/language_picker.gd
@@ -5,29 +5,32 @@
 extends Control
 
 
-func _on_English_pressed() -> void:
+
+
+func _on_english_pressed() -> void:
 	Config.set_lang("en")
 	next_screen()
 
 
-func _on_Portuguese_pressed() -> void:
+func _on_portuguese_pressed() -> void:
 	Config.set_lang("pt")
 	next_screen()
 
 
-func _on_Italian_pressed() -> void:
+func _on_italian_pressed() -> void:
 	Config.set_lang("it")
 	next_screen()
 
 
-func _on_German_pressed() -> void:
+func _on_german_pressed() -> void:
 	Config.set_lang("de")
 	next_screen()
 
 
-func _on_Spanish_pressed() -> void:
+func _on_spanish_pressed() -> void:
 	Config.set_lang("es")
 	next_screen()
 	
 func next_screen() -> void:
 	get_tree().change_scene_to_file("res://src/screens/menu/menu.tscn")
+
