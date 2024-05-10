01d69b309a8e1141b5185481ead640a35a6b522e
Simon Dalvai <info@simondalvai.org>
2024-05-11 00:16:56 +0200

    visual player: add name and number label

--- game/src/match_simulator/match_engine/visual_actors/visual_goalkeeper/visual_goalkeeper.gd
+++ game/src/match_simulator/match_engine/visual_actors/visual_goalkeeper/visual_goalkeeper.gd
@@ -6,17 +6,21 @@ extends Node2D
 class_name VisualGoalkeeper
 
 @onready var body:Sprite2D = $Sprites/Body
+@onready var sprites:Node2D = $Sprites
+@onready var label:Label = $Info/Label
 
 var sim_goal_player:SimGoalkeeper
 var visual_ball:VisualBall
 
 func _process(delta: float) -> void:
 	global_position = global_position.lerp(sim_goal_player.pos, delta * Config.speed_factor * Constants.ticks_per_second)
-	look_at(visual_ball.global_position)
+	sprites.look_at(visual_ball.global_position)
 
 func set_up(p_sim_goal_player:SimGoalkeeper, p_visual_ball:VisualBall, team_color:Color) -> void:
 	sim_goal_player = p_sim_goal_player
 	visual_ball = p_visual_ball
 	global_position = sim_goal_player.pos
 	body.modulate = team_color.darkened(0.5)
+	
+	label.text = str(sim_goal_player.player_res.nr) + " " + (sim_goal_player.player_res.surname)
 	
--- game/src/match_simulator/match_engine/visual_actors/visual_player/visual_player.gd
+++ game/src/match_simulator/match_engine/visual_actors/visual_player/visual_player.gd
@@ -6,16 +6,20 @@ extends Node2D
 class_name VisualPlayer
 
 @onready var body:Sprite2D = $Sprites/Body
+@onready var sprites:Node2D = $Sprites
+@onready var label:Label = $Info/Label
 
 var sim_player:SimPlayer
 var visual_ball:VisualBall
 
 func _process(delta: float) -> void:
 	global_position = global_position.lerp(sim_player.pos, delta * Config.speed_factor * Constants.ticks_per_second)
-	look_at(visual_ball.global_position)
+	sprites.look_at(visual_ball.global_position)
 
 func set_up(p_sim_player:SimPlayer, p_visual_ball:VisualBall, team_color:Color) -> void:
 	sim_player = p_sim_player
 	visual_ball = p_visual_ball
 	global_position = sim_player.pos
 	body.modulate = team_color
+	
+	label.text = str(sim_player.player_res.nr) + " " + (sim_player.player_res.surname)
