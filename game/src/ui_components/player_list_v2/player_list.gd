37455aefad5c49e08eed1053e880c98c39cc571f
Simon Dalvai <info@simondalvai.org>
2024-05-27 21:18:45 +0200

    player list v2: add paginator

--- game/src/ui_components/player_list_v2/player_list.gd
+++ game/src/ui_components/player_list_v2/player_list.gd
@@ -11,11 +11,11 @@ signal select_player(player: Player)
 const PAGE_SIZE:int = 20
 
 @onready var player_rows: VBoxContainer = $PlayerRows
-
-# select filters
 @onready var team_select: OptionButton = $Filters/TeamSelect
 @onready var league_select: OptionButton = $Filters/LeagueSelect
 @onready var pos_select: OptionButton = $Filters/PositionSelect
+@onready var footer: HBoxContainer = $Footer
+@onready var page_indicator: Label = $Footer/PageIndicator
 
 var active_filters: Dictionary = {}
 var active_info_type: int = 0
@@ -24,10 +24,11 @@ var team_search: String = ""
 var all_players: Array[Player] = []
 var visible_players: Array[Player] = []
 
+var page:int
+var page_max:int
+
 
 func _ready() -> void:
-	set_up_players()
-
 	team_select.add_item("NO_TEAM")
 	for league: League in Config.leagues.list:
 		for team: Team in league.teams:
@@ -42,13 +43,28 @@ func _ready() -> void:
 	for league: League in Config.leagues.list:
 		league_select.add_item(league.name)
 	
+	_set_up_all_players()
+
+	page_max = all_players.size() / PAGE_SIZE
+
+	_set_player_rows()
+	_update_page_indicator()
+	
+
+func _set_player_rows() -> void:
+	visible_players = all_players.slice(page * PAGE_SIZE, (page + 1) * PAGE_SIZE)
+	
 	var index: int = 0
 	for player_row: PlayerListRow in player_rows.get_children() as Array[PlayerListRow]:
-		player_row.set_player(visible_players[index])
+		if index < visible_players.size():
+			player_row.visible = true
+			player_row.set_player(visible_players[index])
+		else:
+			player_row.visible = false
 		index += 1
 
 
-func set_up_players(p_reset_options: bool = true) -> void:
+func _set_up_all_players(p_reset_options: bool = true) -> void:
 	if p_reset_options:
 		_reset_options()
 
@@ -57,11 +73,9 @@ func set_up_players(p_reset_options: bool = true) -> void:
 		for team in league.teams:
 			for player in team.players:
 				all_players.append(player)
-	
-	visible_players = all_players.slice(0, PAGE_SIZE)
-	
 
-func remove_player(player_id: int) -> void:
+
+func _remove_player(player_id: int) -> void:
 	active_filters["id"] = player_id
 	_filter_table()
 
@@ -125,5 +139,45 @@ func _reset_options() -> void:
 
 func _on_player_profile_select(player: Player) -> void:
 	select_player.emit(player)
+
+
+func _on_next_2_pressed() -> void:
+	page += 5
+	if page > page_max:
+		page = page_max
+	_update_page_indicator()
+	_set_player_rows()
+
+
+func _on_next_pressed() -> void:
+	page += 1
+	if page > page_max:
+		page = 0
+	_update_page_indicator()
+	_set_player_rows()
+
+
+func _on_prev_pressed() -> void:
+	page -= 1
+	if page < 0:
+		page = page_max
+	_update_page_indicator()
+	_set_player_rows()
+
+
+func _on_prev_2_pressed() -> void:
+	page -= 5
+	if page < 0:
+		page = 0
+	_update_page_indicator()
+	_set_player_rows()
+
+
+func _update_page_indicator() -> void:
+	if all_players.size() <= PAGE_SIZE:
+		footer.hide()
+	else:
+		page_indicator.text = "%d / %d" % [page + 1, page_max + 1]
+
 
 
