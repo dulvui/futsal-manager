de821ab11eeadba63e4ace1d94711f5dce8e2968
Simon Dalvai <info@simondalvai.org>
2024-07-12 21:49:55 +0200

    utils: add idTypes for persons

--- game/src/utils/id_util.gd
+++ game/src/utils/id_util.gd
@@ -3,7 +3,19 @@
 
 extends Node
 
-enum Types { LEAGUE, TEAM, PLAYER, MANAGER, TRANSFER, CONTRACT, EMAIL, MATCH }
+enum Types {
+	LEAGUE,
+	TEAM,
+	PLAYER,
+	MANAGER,
+	AGENT,
+	SCOUT,
+	PRESIDENT,
+	TRANSFER,
+	CONTRACT,
+	EMAIL,
+	MATCH,
+}
 
 
 # max int value is 9223372036854775807, so quite safe to use
