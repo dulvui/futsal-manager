1f3b6351d7f6c9780edffb3c14495a07a99b90e8
Simon Dalvai <info@simondalvai.org>
2024-07-21 08:24:40 +0200

    test: add teams mockups

--- game/src/tests/test_matchengine/test_match_engine.gd
+++ game/src/tests/test_matchengine/test_match_engine.gd
@@ -14,7 +14,9 @@ func test() -> void:
 func test_possess_change() -> void:
 	print("test: possess change...")
 	var match_engine: MatchEngine = MatchEngine.new()
-	match_engine.set_up(Team.new(), Team.new(), 1234)
+	match_engine.set_up(create_mock_team(), create_mock_team(), 1234)
+
+	
 	match_engine.home_team.has_ball = true
 	match_engine.away_team.has_ball = false
 	match_engine.away_team.interception()
@@ -22,3 +24,15 @@ func test_possess_change() -> void:
 	assert(match_engine.home_team.has_ball == false)
 	print("test: possess change done...")
 
+
+func create_mock_team() -> Team:
+	var team: Team = Team.new()
+	team.players = [
+		Player.new(1),
+		Player.new(2),
+		Player.new(3),
+		Player.new(4),
+		Player.new(5)
+	]
+	team.lineup_player_ids = [1, 2, 3, 4, 5]
+	return team
