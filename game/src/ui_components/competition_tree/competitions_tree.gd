922a0fa5dca8646640335ff5b2f37b8583fc429d
Simon Dalvai <info@simondalvai.org>
2024-11-06 14:13:40 +0100

    table: create and use competitions tree

---
+++ game/src/ui_components/competition_tree/competitions_tree.gd
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class_name CompetitionsTree
+extends Tree
+
+signal competition_selected(competition: Competition)
+
+# [competition name][TreeItem]
+var competitions: Dictionary
+var items: Array[TreeItem]
+
+
+func _ready() -> void:
+	theme = ThemeUtil.get_active_theme()
+	competitions = {}
+	items = []
+
+
+func set_up(selected_text: String = "") -> void:
+	# world competitons
+	var world_item: TreeItem = _create_item("WORLD")
+	_create_item("WORLD CUP", world_item)
+	var continents_item: TreeItem = _create_item("CONTINENTS", world_item)
+	# continents
+	for continent: Continent in Global.world.continents:
+		var continent_item: TreeItem = _create_item(continent.name, continents_item)
+		# nations
+		for nation: Nation in continent.nations:
+			var nation_item: TreeItem = _create_item(nation.name, continent_item)
+			# nation leagues
+			for league: League in nation.leagues:
+				_create_item(league.name, nation_item, league)
+			# nation cups
+			_create_item(nation.cup.name, nation_item)
+		# continental cups
+		_create_item(continent.cup_nations.name, continent_item)
+		_create_item(continent.cup_clubs.name, continent_item)
+	
+	# set selected item
+	for item: TreeItem in items:
+		if item.get_text(0) == selected_text:
+			set_selected(item, 0)
+			return
+
+
+func _create_item(text: String, parent: TreeItem = null, competition: Competition = null) -> TreeItem:
+	var item: TreeItem
+	if parent:
+		item = parent.create_child()
+	else:
+		item = create_item()
+	item.set_text(0, text)
+	items.append(item)
+
+	if competition:
+		competitions[competition.name] = competition
+
+	return item
+
+
+func _on_item_mouse_selected(_mouse_position: Vector2, _mouse_button_index: int) -> void:
+	var selected_name: String = get_selected().get_text(0)
+	if competitions.has(selected_name):
+		competition_selected.emit(competitions[selected_name])
+
--- game/src/ui_components/table/table.gd
+++ game/src/ui_components/table/table.gd
@@ -3,36 +3,25 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 class_name VisualTable
-extends VBoxContainer
+extends HBoxContainer
 
-var continent_index: int
-var nation_index: int
-var competition_index: int
+var competition: Competition
 var season_index: int
 var season_amount: int
 
-@onready var grid: GridContainer = $ScrollContainer/GridContainer
-@onready var continents: SwitchOptionButton = $Buttons/Continents
-@onready var nations: SwitchOptionButton = $Buttons/Nations
-@onready var competitions: SwitchOptionButton = $Buttons/Competitions
+@onready var grid: GridContainer = $Positions/ScrollContainer/GridContainer
 @onready var seasons: SwitchOptionButton = $Buttons/Seasons
+@onready var competitions_tree: CompetitionsTree = %CompetitionsTree
 
 
 func _ready() -> void:
-	var continent: Continent = Global.world.get_active_continent()
-	var nation: Nation = Global.world.get_active_nation()
-
-	competition_index = nation.leagues.find(Global.league)
-	continent_index = Global.world.continents.find(continent)
-	nation_index = continent.nations.find(nation)
-
-	continents.set_up(
-		Global.world.continents.map(func(c: Continent) -> String: return c.name), continent_index
-	)
-
 	# start from last entry
 	season_index = Global.league.tables.size() - 1
 	season_amount = Global.league.tables.size()
+
+	competition = Global.league
+	
+	competitions_tree.set_up(competition.name)
 
 	_set_up_seasons()
 	_set_up()
@@ -42,86 +31,75 @@ func _set_up() -> void:
 	# clear grid
 	for child: Node in grid.get_children():
 		child.queue_free()
+	
+	if competition is League:
+		# transform table dictionary to array
+		var table_array: Array[TableValues] = (competition as League).tables[season_index].to_sorted_array()
 
-	var continent: Continent = Global.world.continents[continent_index]
-	var nation: Nation = continent.nations[nation_index]
+		var pos: int = 1
+		for team: TableValues in table_array:
+			var pos_label: Label = Label.new()
+			_style_label(pos_label)
+			pos_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+			pos_label.text = str(pos)
+			pos += 1
+			grid.add_child(pos_label)
 
-	nations.set_up(continent.nations.map(func(n: Nation) -> String: return n.name), nation_index)
+			var name_label: Label = Label.new()
+			name_label.text_overrun_behavior = TextServer.OVERRUN_TRIM_ELLIPSIS
+			name_label.custom_minimum_size = Vector2(310, 0)
+			name_label.text = team.team_name
+			grid.add_child(name_label)
 
-	competitions.set_up(
-		nation.leagues.map(func(l: League) -> String: return l.name), competition_index
-	)
+			var games_played_label: Label = Label.new()
+			_style_label(games_played_label)
+			games_played_label.text = str(team.wins + team.draws + team.lost)
+			grid.add_child(games_played_label)
 
-	var league: League = nation.leagues[competition_index]
+			var wins_label: Label = Label.new()
+			_style_label(wins_label)
+			wins_label.text = str(team.wins)
+			grid.add_child(wins_label)
 
-	var pos: int = 1
+			var draws_label: Label = Label.new()
+			_style_label(draws_label)
+			draws_label.text = str(team.draws)
+			grid.add_child(draws_label)
 
-	# transform table dictionary to array
-	var table_array: Array[TableValues] = league.tables[season_index].to_sorted_array()
+			var lost_label: Label = Label.new()
+			_style_label(lost_label)
+			lost_label.text = str(team.lost)
+			grid.add_child(lost_label)
 
-	for team: TableValues in table_array:
-		var pos_label: Label = Label.new()
-		_style_label(pos_label)
-		pos_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
-		pos_label.text = str(pos)
-		pos += 1
-		grid.add_child(pos_label)
+			var goals_made_label: Label = Label.new()
+			_style_label(goals_made_label)
+			goals_made_label.text = str(team.goals_made)
+			grid.add_child(goals_made_label)
 
-		var name_label: Label = Label.new()
-		name_label.text_overrun_behavior = TextServer.OVERRUN_TRIM_ELLIPSIS
-		name_label.custom_minimum_size = Vector2(310, 0)
-		name_label.text = team.team_name
-		grid.add_child(name_label)
+			var goals_against_label: Label = Label.new()
+			_style_label(goals_against_label)
+			goals_against_label.text = str(team.goals_against)
+			grid.add_child(goals_against_label)
 
-		var games_played_label: Label = Label.new()
-		_style_label(games_played_label)
-		games_played_label.text = str(team.wins + team.draws + team.lost)
-		grid.add_child(games_played_label)
+			var points_label: Label = Label.new()
+			_style_label(points_label)
+			points_label.text = str(team.points)
+			grid.add_child(points_label)
 
-		var wins_label: Label = Label.new()
-		_style_label(wins_label)
-		wins_label.text = str(team.wins)
-		grid.add_child(wins_label)
+			var label_settings: LabelSettings = LabelSettings.new()
+			label_settings.font_size = get_theme_default_font_size()
+			label_settings.font_color = Color.GOLD
 
-		var draws_label: Label = Label.new()
-		_style_label(draws_label)
-		draws_label.text = str(team.draws)
-		grid.add_child(draws_label)
-
-		var lost_label: Label = Label.new()
-		_style_label(lost_label)
-		lost_label.text = str(team.lost)
-		grid.add_child(lost_label)
-
-		var goals_made_label: Label = Label.new()
-		_style_label(goals_made_label)
-		goals_made_label.text = str(team.goals_made)
-		grid.add_child(goals_made_label)
-
-		var goals_against_label: Label = Label.new()
-		_style_label(goals_against_label)
-		goals_against_label.text = str(team.goals_against)
-		grid.add_child(goals_against_label)
-
-		var points_label: Label = Label.new()
-		_style_label(points_label)
-		points_label.text = str(team.points)
-		grid.add_child(points_label)
-
-		var label_settings: LabelSettings = LabelSettings.new()
-		label_settings.font_size = get_theme_default_font_size()
-		label_settings.font_color = Color.GOLD
-
-		if team.team_name == Global.team.name:
-			pos_label.label_settings = label_settings
-			name_label.label_settings = label_settings
-			games_played_label.label_settings = label_settings
-			wins_label.label_settings = label_settings
-			draws_label.label_settings = label_settings
-			lost_label.label_settings = label_settings
-			goals_made_label.label_settings = label_settings
-			goals_against_label.label_settings = label_settings
-			points_label.label_settings = label_settings
+			if team.team_name == Global.team.name:
+				pos_label.label_settings = label_settings
+				name_label.label_settings = label_settings
+				games_played_label.label_settings = label_settings
+				wins_label.label_settings = label_settings
+				draws_label.label_settings = label_settings
+				lost_label.label_settings = label_settings
+				goals_made_label.label_settings = label_settings
+				goals_against_label.label_settings = label_settings
+				points_label.label_settings = label_settings
 
 
 func _set_up_seasons() -> void:
@@ -147,22 +125,7 @@ func _on_seasons_item_selected(index: int) -> void:
 	_set_up()
 
 
-func _on_continents_item_selected(index: int) -> void:
-	continent_index = index
-	nations.option_button.selected = 0
-	nation_index = 0
-	competitions.option_button.selected = 0
-	competition_index = 0
+func _on_competitions_tree_competition_selected(p_competition: Competition) -> void:
+	competition = p_competition
 	_set_up()
 
-
-func _on_nations_item_selected(index: int) -> void:
-	nation_index = index
-	competitions.option_button.selected = 0
-	competition_index = 0
-	_set_up()
-
-
-func _on_competitions_item_selected(index: int) -> void:
-	competition_index = index
-	_set_up()
