eb0d613a0f67763ee4a1ab93044d86c48290b725
Simon Dalvai <info@simondalvai.org>
2024-11-22 19:17:57 +0100

    dashboard: simulate other matches on thread during match

--- game/src/screens/dashboard/dashboard.gd
+++ game/src/screens/dashboard/dashboard.gd
@@ -234,6 +234,9 @@ func _on_next_match_button_pressed() -> void:
 
 func _next_day() -> void:
 	if match_ready:
+		# simulate other matches in background
+		ThreadUtil.random_results()
+		
 		get_tree().change_scene_to_file("res://src/screens/match/match.tscn")
 		return
 
@@ -261,7 +264,7 @@ func _next_day() -> void:
 		next_match_button.hide()
 	elif Global.world.calendar.day().matches.size() > 0:
 		#simulate all other matches
-		LoadingUtil.start("SIMULATIN_RESULTS", LoadingUtil.Type.MATCH_RESULTS, true)
+		LoadingUtil.start("SIMULATING_RESULTS", LoadingUtil.Type.MATCH_RESULTS, true)
 		loading_screen.show()
 		ThreadUtil.random_results()
 		#Global.world.random_results()
