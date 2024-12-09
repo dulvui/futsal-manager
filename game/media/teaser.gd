b779816ed7cf75bcbc35619c847c7cf2f502cd83
Simon Dalvai <info@simondalvai.org>
2024-12-09 12:05:00 +0100

    media: create firs trailer movie scene

---
+++ game/media/teaser.gd
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+extends Control
+
+
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+
+func _ready() -> void:
+	theme = ThemeUtil.get_active_theme()
+	# Tests.setup_mock_world(true)
+	animation_player.play("Teaser")
+
+
+func _process(_delta: float) -> void:
+	pass
+
