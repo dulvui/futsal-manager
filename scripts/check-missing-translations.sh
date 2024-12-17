c9d4d5936146f2143929738eeadfefaae63fdd88
Simon Dalvai <info@simondalvai.org>
2024-12-17 22:00:56 +0100

    scripts: also grep strings with underscores

--- scripts/check-missing-translations.sh
+++ scripts/check-missing-translations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-grep -rho '\b[[:upper:]]\+\b' ../game/src/ | sort | uniq > found
-grep -rho '\b[[:upper:]]\+\b' ../game/translations/translations.csv | sort | uniq > available
+grep -rho '\b[[:upper:]_]\+\b' ../game/src/ | sort | uniq > found
+grep -rho '\b[[:upper:]_]\+\b' ../game/translations/translations.csv | sort | uniq > available
 
 grep -v -F -x -f available found > missing
 
