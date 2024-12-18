6fa5d7bf9067dd58b50595e864936be422cd86db
Simon Dalvai <info@simondalvai.org>
2024-12-18 18:19:01 +0100

    reuse: add license for new scripts

--- scripts/check-missing-translations.sh
+++ scripts/check-missing-translations.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 
 grep -rho '\b[[:upper:]_]\+\b' ../game/src/ | sort | uniq > found
 grep -rho '\b[[:upper:]_]\+\b' ../game/translations/translations.csv | sort | uniq > available
